a19bd309b357e1bfb83146d2f0a366632b38c1eb
ahephner <ajhephner178@gmail.com>
2019-05-17 15:33:16 -0400

    search timeout, controller good display bad

--- force-app/main/default/lwc/productSearch/productSearch.js
+++ force-app/main/default/lwc/productSearch/productSearch.js
@@ -1,3 +1,29 @@
-import { LightningElement } from 'lwc';
+/* eslint-disable no-console */
+//import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
+//import { fireEvent } from 'c/pubsub';
+import { LightningElement, track, wire } from 'lwc';
+import searchGoods from '@salesforce/apex/searchProds.searchGoods'; 
+export default class ProductSearch extends LightningElement {
+        @track searchProd;
+        @track goods;
 
-export default class ProductSearch extends LightningElement {}
+        @wire(searchGoods,{searchProd: '$searchProd'})
+         
+        loadGoods(results){
+            this.goods = results;
+                // if(results.data){
+                //     fireEvent(this.pageRef, 'productListUpdate', results.data); 
+                // }
+        }
+        searchChange(event){
+            window.clearTimeout(this.delayTimeout);
+            const searchProd = event.target.value; 
+            // eslint-disable-next-line @lwc/lwc/no-async-operation
+            this.delayTimeout = setTimeout(() =>{
+                this.searchProd = searchProd;
+            }, 300); 
+              
+            
+        }
+
+}
