f5672ab6b38eae7e918ee1fad0ab37c6f606b7e3
ahephner <ajhephner178@gmail.com>
2019-05-20 10:53:07 -0400

    now passing data to the dom

--- force-app/main/default/lwc/productSearch/productSearch.js
+++ force-app/main/default/lwc/productSearch/productSearch.js
@@ -1,19 +1,33 @@
 /* eslint-disable no-console */
-//import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
+import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
 //import { fireEvent } from 'c/pubsub';
 import { LightningElement, track, wire } from 'lwc';
 import searchGoods from '@salesforce/apex/searchProds.searchGoods'; 
+    const columns = [
+        {label:'Product #', fieldName: 'Name'},
+        {label: 'Name', fieldName:'Product_Name__c'}, 
+        {label: 'Description', fieldName:"Product_Description__c"},
+    ];
 export default class ProductSearch extends LightningElement {
         @track searchProd;
         @track goods;
+        @track columns = columns; 
+        @track tableLoadingState = true; 
 
+        @wire(CurrentPageReference) pageRef; 
         @wire(searchGoods,{searchProd: '$searchProd'})
          
-        loadGoods(results){
-            this.goods = results;
-                // if(results.data){
-                //     fireEvent(this.pageRef, 'productListUpdate', results.data); 
-                // }
+        loadGoods(result){
+            this.goods = result;
+            // eslint-disable-next-line no-undef
+            this.tableLoadingState = false; 
+            //pubsub needs to be added 
+                // if(result.data){
+                  //   fireEvent(this.pageRef, 'productListUpdate', result.data); 
+                 //}
+        }
+        connectedCallback() {
+            
         }
         searchChange(event){
             window.clearTimeout(this.delayTimeout);
@@ -22,8 +36,19 @@ export default class ProductSearch extends LightningElement {
             this.delayTimeout = setTimeout(() =>{
                 this.searchProd = searchProd;
             }, 300); 
-              
             
         }
 
-}
+        
+            handleProductView(event) {
+                // Navigate to bear record page
+                this[NavigationMixin.Navigate]({
+                    type: 'standard__recordPage',
+                    attributes: {
+                        recordId: event.target.good.Id,
+                        objectApiName: 'Product__c',
+                        actionName: 'view',
+                    },
+                });
+        }
+    }
