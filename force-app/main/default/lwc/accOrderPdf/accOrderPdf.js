abda575f8af0f1fd7eb01831b2436595ddfcedb1
ahephner <ajhephner178@gmail.com>
2019-06-10 14:19:50 -0400

    front end done passing values to mid

--- force-app/main/default/lwc/accOrderPdf/accOrderPdf.js
+++ force-app/main/default/lwc/accOrderPdf/accOrderPdf.js
@@ -1,14 +1,12 @@
 import { LightningElement, track } from 'lwc';
+//need apex class 
+export default class App extends LightningElement {
+   //@api recordid
+    @track dateOne ='';
 
-export default class AccOrderPdf extends LightningElement {
-//get current Record page id?
-    @api recordId; 
-    @track
-    dateOne ='';
-
-    @track
-    dateTwo;
+    @track dateTwo;
     @track prodNum
+    @track yn = false; 
     dateOneChange(event){
         this.dateOne = event.target.value;
     }
@@ -18,8 +16,13 @@ export default class AccOrderPdf extends LightningElement {
     prodNumChange(e){
         this.prodNum = e.target.value;
     }
-   handleSearch(){
-       //this.dateOne = dateOne;
-       //bind to apex here use wrapper 
-   }
+    ynChange(e){
+        this.yn = e.target.checked; 
+    }
+//    handleSearch(){
+//        this.dateOne = dateOne;
+//        this.dateTwo = dateTwo;
+//        this.prodNum = prodNum; 
+//    }
+   
 }
