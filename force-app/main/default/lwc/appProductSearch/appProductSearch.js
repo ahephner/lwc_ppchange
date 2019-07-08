55ca862d1ea38a71857f5e5e6b42bf8a5b85852f
ahephner <ajhephner178@gmail.com>
2019-07-08 16:15:48 -0400

    going to split out area tab

--- force-app/main/default/lwc/appProductSearch/appProductSearch.js
+++ force-app/main/default/lwc/appProductSearch/appProductSearch.js
@@ -1,13 +1,16 @@
 /* eslint-disable no-console */
 import { LightningElement, track, api, wire } from 'lwc';
 import getAreas from '@salesforce/apex/appProduct.getAreas'; 
-
+import searchProduct from '@salesforce/apex/appProduct.searchProduct'
 
 export default class AppProductSearch extends LightningElement {
-    @track value= '';
+    @track cat = 'ALL';
     @track area; 
     @api recordId; 
-    
+    @track searchKey;
+    @track cat;
+    @track goods; 
+   //this needs it's own card  
     @wire(getAreas,{recordId: '$recordId'})
         areaList;
      
@@ -15,19 +18,38 @@ export default class AppProductSearch extends LightningElement {
          console.log('==> '+JSON.stringify(this.areaList.data)); 
          return this.areaList.data; 
      }   
+
+     selectArea(e){
+        this.value = e.detail.value 
+    }
+
+///see above
+
      get pc() {
         return [
-            {label: 'Herbicide', value:'Herbicide'},
-            {label: 'Fungicide', value:'Fungicide'},
-            {label: 'Insecticide', value:'Insecticide'},
-            {label: 'PGR', value:'PGR'}, 
+            {label: 'All', value:'ALL'},
+            {label: 'Herbicide', value:'Chemicals-Herbicide'},
+            {label: 'Fungicide', value:'Chemicals-Fungicide'},
+            {label: 'Insecticide', value:'Chemicals-Insecticide'},
+            {label: 'PGR', value:'Chemicals-Growth Regulator'}, 
         ]; 
      }
      selectCat(e){
-         this.value = e.detail.value; 
-
+         this.cat = e.detail.value; 
      }
-     selectArea(e){
-         this.value = e.detail.value 
-     }
+   @wire(searchProduct, {searchKey: '$searchKey', cat:'$cat'})
+        loadProd(results){
+            this.goods = results; 
+            console.log(this.goods);
+            console.log('here is search key '+this.searchKey)
+            console.log(this.cat);
+        }
+    searchProd(event){
+        window.clearTimeout(this.delayTimeout);
+            const searchKey = event.target.value; 
+            // eslint-disable-next-line @lwc/lwc/no-async-operation
+            this.delayTimeout = setTimeout(() =>{
+                this.searchKey = searchKey;
+            }, 300);
+    }
 }
