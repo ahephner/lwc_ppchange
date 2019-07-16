f267eb46a116bb549745d371cd014d008da6b285
ahephner <ajhephner178@gmail.com>
2019-07-16 09:24:19 -0400

    adding multiple lines into app info and rate

--- force-app/main/default/lwc/appInfo/appInfo.js
+++ force-app/main/default/lwc/appInfo/appInfo.js
@@ -3,20 +3,27 @@ import { CurrentPageReference } from 'lightning/navigation';
 import {getRecord, getFieldValue } from 'lightning/uiRecordApi'
 import { registerListener, unregisterAllListeners } from 'c/pubsub';
 
+import PRODUCT_ID from '@salesforce/schema/Product__c.Id'
 import PRODUCT_NAME from '@salesforce/schema/Product__c.Product_Name__c'
-
-const fields = [PRODUCT_NAME];
+//may need to also query product__c id
+const fields = [PRODUCT_NAME, PRODUCT_ID];
 export default class AppInfo extends LightningElement {
     recordId; 
 
     @track name; 
+    @track productId;  
+    @track newProds = [{Id:'', name: ''}]; 
 
     @wire(CurrentPageReference) pageRef;
 
     @wire(getRecord, {recordId: '$recordId', fields})
     wiredProduct({error, data}){
         if(data){
-            this.name = getFieldValue(data, PRODUCT_NAME); 
+            this.newProds = [
+            ...this.newProds, {
+            Id: this.name = getFieldValue(data, PRODUCT_ID), 
+            name: this.productId = getFieldValue(data, PRODUCT_NAME) 
+            }]; 
             this.error = undefined;
         }else if (error){
             this.error = error;
