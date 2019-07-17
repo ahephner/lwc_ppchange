2d41ad479b933ce8a8c6f7af5a614830b0daac44
ahephner <ajhephner178@gmail.com>
2019-07-17 10:33:35 -0400

    before splitting out insert

--- force-app/main/default/lwc/appInfo/appInfo.js
+++ force-app/main/default/lwc/appInfo/appInfo.js
@@ -1,33 +1,39 @@
 /* eslint-disable no-console */
 import { LightningElement, track, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
-import {getRecord, getFieldValue } from 'lightning/uiRecordApi'
+import {getRecord, getFieldValue, createRecord } from 'lightning/uiRecordApi'
 import { registerListener, unregisterAllListeners } from 'c/pubsub';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { reduceErrors } from 'c/idsUtils';
 
+import APP_OBJECT from '@salesforce/schema/Application__c'
+import APP_NAME from '@salesforce/schema/Application__c.Name'
+import AREA from '@salesforce/schema/Application__c.Area__c'
 import PRODUCT_ID from '@salesforce/schema/Product__c.Id'
 import PRODUCT_NAME from '@salesforce/schema/Product__c.Product_Name__c'
 
-const fields = [PRODUCT_NAME, PRODUCT_ID];
+const xfields = [PRODUCT_NAME, PRODUCT_ID];
 export default class AppInfo extends LightningElement {
     recordId; 
-
+    @track appId; 
     @track appName;
     @track appDate; 
     @track areaId; 
-    @track name; 
+    @track name;
+    @track rate;  
     @track productId;  
     @track newProds = [{}]; 
      
     @wire(CurrentPageReference) pageRef;
 
-    @wire(getRecord, {recordId: '$recordId', fields})
+    @wire(getRecord, {recordId: '$recordId', xfields})
     wiredProduct({error, data}){
         if(data){
             this.newProds = [
             ...this.newProds, {
-            Id: this.name = getFieldValue(data, PRODUCT_ID), 
-            name: this.productId = getFieldValue(data, PRODUCT_NAME), 
-            rate:0 
+            Id: this.productId = getFieldValue(data, PRODUCT_ID), 
+            name: this.name = getFieldValue(data, PRODUCT_NAME), 
+            rate:0
             }]; 
            
             this.error = undefined;
@@ -39,7 +45,7 @@ export default class AppInfo extends LightningElement {
 
         connectedCallback(){
             // eslint-disable-next-line no-console
-            console.log('callback')
+            //console.log('callback')
             registerListener('productSelected', this.handleProductSelected, this); 
             registerListener('areaSelect', this.handleNewArea, this);
         }
@@ -62,18 +68,44 @@ export default class AppInfo extends LightningElement {
 
     newName(e){
         this.appName = e.detail.value; 
+        this.appId = undefined;
     }
-    rate(e){
-        this.newProds.rate = e.detail.value 
-    }
-    insertApp(){
-           this.newProds.forEach(function(x){
-               console.log('loop ' +x.name); 
-           })
-        console.log(this.newProds);
-        console.log('area ' + this.areaId);
-        console.log('date '+ this.appDate);
-        console.log('app name ' + this.appName)
-    }
+    // rate(e){
+    //     this.newProds.rate = e.detail.value
 
+    // }
+    createApplication__c(){
+        console.log('area ' + this.areaId);
+        // console.log('date '+ this.appDate);
+        console.log('app name ' + this.appName)
+        const fields = {}
+        fields[AREA.fieldApiName] = this.areaId
+        fields[APP_NAME.fieldApiName] = this.appName
+        console.log(fields)
+        const recordInput = {apiName: APP_OBJECT.objectApiName, fields};
+        
+        createRecord(recordInput)
+            .then(application__c => {
+                this.appId = application__c.id; 
+                console.log('new appID ' +application__c)
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'App created',
+                        variant: 'success',
+                    }),
+                );
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error creating record',
+                        message: reduceErrors(error).join(', '),
+                        variant: 'error',
+                       
+                    }),
+                ); 
+            });
+    }
 }
+
