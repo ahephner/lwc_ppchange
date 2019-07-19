ed9f7cd3a2596a0afeeef3fb451c4d8aac90bb16
ahephner <ajhephner178@gmail.com>
2019-07-19 16:19:37 -0400

    saving end oday friday

--- force-app/main/default/lwc/appInfo/appInfo.js
+++ force-app/main/default/lwc/appInfo/appInfo.js
@@ -3,8 +3,8 @@ import { LightningElement, track, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
 import {getRecord, getFieldValue } from 'lightning/uiRecordApi'
 import { registerListener, unregisterAllListeners } from 'c/pubsub';
-import addApplication from '@salesforce/apex/addApp.addApplication'
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+//import addApplication from '@salesforce/apex/addApp.addApplication'
+//import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 import PRODUCT_ID from '@salesforce/schema/Product__c.Id'
 import PRODUCT_NAME from '@salesforce/schema/Product__c.Product_Name__c'
@@ -17,20 +17,23 @@ export default class AppInfo extends LightningElement {
     @track appDate; 
     @track areaId; 
     @track name;
-    @track rate;  
+    //@track rate;  
     @track productId;  
     @track newProds = [{}]; 
-     
+    lastId = 0; 
     @wire(CurrentPageReference) pageRef;
 
     @wire(getRecord, {recordId: '$recordId', fields})
     wiredProduct({error, data}){
         if(data){
+            this.lastId = this.lastId + 1; 
+            console.log(this.lastId)
             this.newProds = [
-            ...this.newProds, {
+            ...this.newProds, {  
             Id: this.productId = getFieldValue(data, PRODUCT_ID), 
             name: this.name = getFieldValue(data, PRODUCT_NAME), 
-            rate:0
+            rate: "0", 
+            numb:1
             }]; 
            
             this.error = undefined;
@@ -61,37 +64,47 @@ export default class AppInfo extends LightningElement {
     }
     date(e){
         this.appDate = e.detail.value; 
+        
     }
 
     newName(e){
         this.appName = e.detail.value; 
         this.appId = undefined;
     }
-    // rate(e){
-    //     this.newProds.rate = e.detail.value
-
-    // }
+    newRate(e){
+        this.newProds.forEach(function(p, i,d){
+            console.log(p)
+            this[i].Id = this[i].Id; 
+            this[i].name = this[i].name; 
+            this[i].rate = e.detail.value; 
+            console.log(d)
+        }, this.newProds) 
+             console.log(this.newProds)
+    }
     createApplication__c(){
-       let params = {
-           appName: this.appName,
-           appArea: this.areaId,
-           appDate: this.appDate
-       };
-       console.log(params)
-        addApplication({wrapper:params})
-            .then((resp)=>{
-                this.appId = resp.Id; 
-                console.log(this.appId);
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Success',
-                        message: 'Look at the applications tab!',
-                        variant: 'success'
-                    })
-                );
-            }).catch((error)=>{
-                console.log(JSON.stringify(error)); 
-            })
+     console.log(this.newProds)
+       
+       
+    //     let params = {
+    //        appName: this.appName,
+    //        appArea: this.areaId,
+    //        appDate: this.appDate
+    //    };
+    //    console.log(params)
+    //     addApplication({wrapper:params})
+    //         .then((resp)=>{
+    //             this.appId = resp.Id; 
+    //             console.log(this.appId);
+    //             this.dispatchEvent(
+    //                 new ShowToastEvent({
+    //                     title: 'Success',
+    //                     message: 'Look at the applications tab!',
+    //                     variant: 'success'
+    //                 })
+    //             );
+    //         }).catch((error)=>{
+    //             console.log(JSON.stringify(error)); 
+    //         })
     }
 }
 
