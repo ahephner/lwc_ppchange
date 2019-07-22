44a6a4c6ce28cb610a3beafc6fbced63b472d863
ahephner <ajhephner178@gmail.com>
2019-07-22 15:46:17 -0400

    updating rate in browser

--- force-app/main/default/lwc/appInfo/appInfo.js
+++ force-app/main/default/lwc/appInfo/appInfo.js
@@ -12,6 +12,7 @@ import PRODUCT_NAME from '@salesforce/schema/Product__c.Product_Name__c'
 const fields = [PRODUCT_NAME, PRODUCT_ID];
 export default class AppInfo extends LightningElement {
     recordId; 
+
     @track appId; 
     @track appName;
     @track appDate; 
@@ -19,7 +20,7 @@ export default class AppInfo extends LightningElement {
     @track name;
     //@track rate;  
     @track productId;  
-    @track newProds = [{}]; 
+    @track newProds = [{}]
     lastId = 0; 
     @wire(CurrentPageReference) pageRef;
 
@@ -27,22 +28,23 @@ export default class AppInfo extends LightningElement {
     wiredProduct({error, data}){
         if(data){
             this.lastId = this.lastId + 1; 
-            console.log(this.lastId)
-            this.newProds = [
-            ...this.newProds, {  
-            Id: this.productId = getFieldValue(data, PRODUCT_ID), 
-            name: this.name = getFieldValue(data, PRODUCT_NAME), 
-            rate: "0", 
-            numb:1
-            }]; 
-           
+            //console.log(this.lastId)
+            this.newProds= [
+            ...this.newProds,   
+             { Id:   this.productId = getFieldValue(data, PRODUCT_ID), 
+              name:  this.name = getFieldValue(data, PRODUCT_NAME), 
+              rate:  "0", 
+              numb: this.lastId 
+             }]; 
+            
             this.error = undefined;
         }else if (error){
             this.error = error;
             this.name = undefined;
         }
+    console.log(this.newProds)
     }
-
+    
         connectedCallback(){
             // eslint-disable-next-line no-console
             //console.log('callback')
@@ -71,19 +73,21 @@ export default class AppInfo extends LightningElement {
         this.appName = e.detail.value; 
         this.appId = undefined;
     }
+    get newProdsSelect(){
+        return this.newProds.length >= 1; 
+    }
     newRate(e){
-        this.newProds.forEach(function(p, i,d){
-            console.log(p)
-            this[i].Id = this[i].Id; 
-            this[i].name = this[i].name; 
-            this[i].rate = e.detail.value; 
-            console.log(d)
-        }, this.newProds) 
-             console.log(this.newProds)
+     let index = this.newProds.findIndex(prod => prod.numb === e.target.name)
+     //console.log("index number" + index);
+     this.newProds[index].rate = e.detail.value;    
+    // console.log('detail. value')
+    // console.log(e.detail.value) 
+    // console.log('newProds update')
+    // console.log(this.newProds[index])
     }
     createApplication__c(){
      console.log(this.newProds)
-       
+   
        
     //     let params = {
     //        appName: this.appName,
