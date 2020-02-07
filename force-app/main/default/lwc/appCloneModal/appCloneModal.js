f85d18902596c9765bda4b380c501afc0943e82c
ahephner <ajhephner178@gmail.com>
2020-02-07 14:26:46 -0500

    done friday

--- force-app/main/default/lwc/appCloneModal/appCloneModal.js
+++ force-app/main/default/lwc/appCloneModal/appCloneModal.js
@@ -4,10 +4,10 @@ import { CurrentPageReference } from 'lightning/navigation';
 import { registerListener, unregisterListener } from 'c/pubsub';
 import cloneProducts from '@salesforce/apex/ProgramCloneWithApps_Controller.cloneProducts';
 const columns = [
-    { label: 'Name', fieldName: 'Product_Name__c',  cellAttributes: { alignment: 'left' } },
+    { label: 'Name', fieldName: 'Product_Name__c',  cellAttributes: { alignment: 'left' },  sortable: "true" },
     { label: 'Rate', fieldName: 'Report_Rate__c',   cellAttributes: { alignment: 'left' }},
-    { label: 'Price', fieldName: 'Unit_Price__c', type:'currency',  cellAttributes: { alignment: 'left' }},
-    { label: 'Margin', fieldName: 'Margin__c', type:'percent',  cellAttributes: { alignment: 'left' }},
+    { label: 'Price', fieldName: 'Unit_Price__c', type:'currency',  cellAttributes: { alignment: 'left' },  sortable: "true"},
+    { label: 'Margin %', fieldName: 'Margin__c', type:'number',  cellAttributes: { alignment: 'left'},  sortable: "true"},
 
 ];
 export default class AppCloneModal extends LightningElement {
@@ -64,5 +64,43 @@ export default class AppCloneModal extends LightningElement {
     }
     closeAppClone(){
         this.openAppClone = false; 
+    }
+
+    //sorting
+    handleSortdata(event) {
+        // field name
+        this.sortBy = event.detail.fieldName;
+
+        // sort direction
+        this.sortDirection = event.detail.sortDirection;
+
+        // calling sortdata function to sort the data based on direction and selected field
+        this.sortData(event.detail.fieldName, event.detail.sortDirection);
+    }
+
+    sortData(fieldname, direction) {
+        // serialize the data before calling sort function
+        let parseData = JSON.parse(JSON.stringify(this.data));
+
+        // Return the value stored in the field
+        let keyValue = (a) => {
+            return a[fieldname];
+        };
+
+        // cheking reverse direction 
+        let isReverse = direction === 'asc' ? 1: -1;
+
+        // sorting data 
+        parseData.sort((x, y) => {
+            x = keyValue(x) ? keyValue(x) : ''; // handling null values
+            y = keyValue(y) ? keyValue(y) : '';
+
+            // sorting values based on direction
+            return isReverse * ((x > y) - (y > x));
+        });
+
+        // set the sorted data to data table data
+        this.data = parseData;
+
     }
 }
