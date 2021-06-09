4de84af4044cc9d2d8efbf57674c4025a00e052a
ahephner <ajhephner178@gmail.com>
2021-06-09 15:16:20 -0400

    table on desktop version cards on mobile. Can save and validate on the table

--- force-app/main/default/lwc/specialOrderProduct/specialOrderProduct.js
+++ force-app/main/default/lwc/specialOrderProduct/specialOrderProduct.js
@@ -2,12 +2,15 @@ import { LightningElement, api, wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import FORM_FACTOR from '@salesforce/client/formFactor';
 import getItems from '@salesforce/apex/specialOrderCloneWProducts.getOrderRequestItems';
+import { updateRecord } from 'lightning/uiRecordApi';
+import { refreshApex } from '@salesforce/apex';
+
 const columns = [
-    {label:'Product Requested', 'fieldName':'product', type:'text' },
+    {label:'Product Requested', 'fieldName':'nameURL', type:'url', typeAttributes:{label:{fieldName:'product'}},target:'_blank' },
     {label:'QTY', 'fieldName':'Quantity_Requested__c', type:'number' },
     {label:'Unit Cost', 'fieldName':'Unit_Cost__c', type:'currency'},    
-    {label:'Min Margin', 'fieldName':'Minimum_Margin__c', type:'percent', editable:true },
-    {label:'Sales Margin', 'fieldName':'Sales_Margin__c', type:'percent',  editable:true},
+    {label:'Min Margin', 'fieldName':'Minimum_Margin__c', type:'percent-fixed' },
+    {label:'Sales Margin', 'fieldName':'Sales_Margin__c', type:'percent-fixed',  editable:true},
     {label:'Price', 'fieldName':'Unit_Price__c', type:'currency', editable:true},
 ]
 export default class SpecialOrderProduct extends LightningElement {
@@ -34,11 +37,11 @@ export default class SpecialOrderProduct extends LightningElement {
             this.requestItems = result;
             if(result.data){
                 let product; 
+                let nameURL
                 this.items = result.data.map(row =>{
                     product = row.ATS_Product__c ? row.ATS_Product__r.Product_Name__c : row.Product_Description__c;
-                    row.Minimum_Margin__c = row.Minimum_Margin__c/100;
-                    row.Sales_Margin__c = row.Sales_Margin__c/100; 
-                    return {...row, product}
+                    nameURL = `/${row.Id}`;
+                    return {...row, nameURL, product}
                 })
                 console.log(this.items);
                 
@@ -48,7 +51,55 @@ export default class SpecialOrderProduct extends LightningElement {
                 console.log(result.error);
             }
         }
+//Refresh
+        @api
+        async refresh(){
+            this.isLoading = true;
+            console.log('refresh');
+            await refreshApex(this.requestItems);
+            this.isLoading = false;
+        }
      
+        //save From the Table
+        handleSave(event){
+            this.isLoading = true;
+        
+            const recordInputs =  event.detail.draftValues.slice().map(draft => {
+                const fields = Object.assign({}, draft);
+                return { fields };
+            });
+            const promises = recordInputs.map(recordInput => updateRecord(recordInput));
+            Promise.all(promises).then(prod => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Ship It!',
+                        variant: 'success'
+                    })
+                );
+                    
+                
+                 // Display fresh data in the datatable
+               return this.refresh();
+            }).catch(error => {
+                console.log(error);
+                
+                // Handle error
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Margin Error',
+                        message: error.body.output.errors[0].message,
+                        variant: 'error'
+                    })
+                )
+            }).finally(() => {
+                console.log('finally');
+                
+                this.draftValues = []; 
+                this.isLoading = false
+                
+            });  
+        } 
 
 
 }
